d2ebfb891140351038ba59f950278b6ee551695c
Morii Huang <miaochaoe@163.com>
2021-12-01 15:44:11 +0800

    second Upgrade

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from flask import Flask,render_template,request,redirect,url_for
 import pymysql
 import json
 import requests
+import json,random
+with open("2.json","rb") as f:
+    b=json.load(f)
 app=Flask(__name__)
 app.jinja_env.variable_start_string = '{['
 app.jinja_env.variable_end_string = ']}'
-@app.route('/')
+@app.route('/home')
 def vue():
     return render_template('final_project1.html')
+@app.route('/a')
+def jsonpic():
+    return json.dumps([urls for urls in random.sample(b["data"],20) if "master" in urls["src"]])
+@app.route('/picture')
+def showpicture():
+    return render_template("showpic.html",initli=[urls["src"] for urls in random.sample(b["data"],20) if "master" in urls["src"]])
 app.run(host='0.0.0.0',debug=True);
---
+++ static/main.js
@@ -0,0 +1,9 @@
+const projectPath='http://10.166.224.160:5000/picture'
+    const app = new Vue({
+    el:"#app",
+	data:{
+	isShow:1,
+	isRegister:0,
+	},
+	methods:{}
+    })
